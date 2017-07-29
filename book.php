9ce68cec8e33740b185d9b506b2d6344b9d4abb8
K. Ouafi <kouafi@icloud.com>
2017-07-29 10:47:44 +0100

    fix concatenation

--- book.php
+++ book.php
@@ -1,19 +1,21 @@
 <?php
 
+date_default_timezone_set('GMT'); // set default time zone
+
 $storeId = ""; // store id
 $privateKey = ""; // private key
+
 try {
   if (isset($_POST['code'], $_POST['orderCheckSum']) {
     $code = $_POST['code'];
     $order_check_sum = $_POST['orderCheckSum'];
-    if (md5("PRE-PAY", $_POST['amount'], $storeId, $_POST['externalId'], $_POST['buyerEmail'], $privateKey) == $order_check_sum) {
+    if (md5("PRE-PAY" . $_POST['amount'] . $storeId, $_POST['externalId'] . $_POST['buyerEmail'] . $privateKey) == $order_check_sum) {
       // insérer le code binga dans votre base de données avec un status pending
       // Ne pas insérer des variables $_POST directement à la base de données, pensez à échapper les caractères spéciaux.
     } else {
-      die();
+      throw new Exception("Checksums do not match!");
     }
   }
-
 } catch (Exception $e) {
   echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
